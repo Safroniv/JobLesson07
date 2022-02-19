b15b04b7ac8f168c52bf00d1879713c8b2901be8
Safroniv <safroniv@mail.ru>
2022-02-19 11:39:57 +0300

    Урок 7 Задание 1 - Декомпиляция через dotPeek

---
+++ JobLesson07-DotPeek/JobLesson07Part01-DotPeek/Program.cs
@@ -0,0 +1,15 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: JobLesson07Part01.Program
+// Assembly: JobLesson07Part01, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 6B412A88-969A-4AF8-9CF2-8654363D27A9
+// Assembly location: C:\Users\Safjon\YandexDisk\++Programming\++LernCShrp++\Proj_GeekBrains\01-1qua-3EntrCShrp\Lesn07\JobLesson07\JobLesson07Part01\bin\Debug\net5.0\JobLesson07Part01.dll
+
+using System;
+
+namespace JobLesson07Part01
+{
+  internal class Program
+  {
+    private static void Main() => Console.WriteLine("Привет Мир! - это решение через декомпиляцию в DotPeek");
+  }
+}
