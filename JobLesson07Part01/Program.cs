292480db998ae484f3b41b4f4474c53d1b9b7db5
Safroniv <safroniv@mail.ru>
2022-02-19 11:31:38 +0300

    Урок 7 Задание 1

--- JobLesson07Part01/Program.cs
+++ JobLesson07Part01/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-using System.Text;
 
 namespace JobLesson07Part01
 {
+    //Урок 7 Задание 1
+    //Написать любое приложение, произвести его сборку с помощью MSBuild, осуществить
+    //декомпиляцию с помощью dotPeek, внести правки в программный код и пересобрать приложение.
     internal class Program
     {
         static void Main()
