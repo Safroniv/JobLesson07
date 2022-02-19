f97bc805bcdbc4631e1c0e104af5a5e50f2bfcfb
Safroniv <safroniv@mail.ru>
2022-02-19 11:29:18 +0300

    Добавьте файлы проекта.

---
+++ JobLesson07Part01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace JobLesson07Part01
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Привет Мир!");
+        }
+    }
+}
